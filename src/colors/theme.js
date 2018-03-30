16cde73070a19b0f6597d8d0d4c06b3e7ef9d4c2
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-03-30 20:23:02 +0200

    chore: Define UI theme variant

---
+++ src/colors/theme.js
@@ -0,0 +1,11 @@
+import colors from 'vuetify/es5/util/colors';
+
+export default {
+  primary: colors.shades.white,
+  secondary: colors.grey.darken4,
+  accent: colors.lightBlue.darken1,
+  error: colors.red.accent3,
+  warning: colors.amber.base,
+  info: colors.blue.lighten1,
+  success: colors.green.darken1
+};
